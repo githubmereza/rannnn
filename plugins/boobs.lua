d8319730f83d1e677266418b61d1351199811132
githubmereza <github09355332076@gmail.com>
2016-02-17 18:37:00 +0330

    y

--- plugins/boobs.lua
+++ plugins/boobs.lua
@@ -59,8 +59,8 @@ end
 return {
   description = "Gets a random boobs or butts pic", 
   usage = {
-    "!boobs: Get a boobs NSFW image. Ù‹Úºâ€â€",
-    "!butts: Get a butts NSFW image. Ù‹Úºâ€â€"
+    "boobs: Get a boobs NSFW image. Ù‹Úºâ€â€",
+    "butts: Get a butts NSFW image. Ù‹Úºâ€â€"
   },
   patterns = {
     "^boobs$",
